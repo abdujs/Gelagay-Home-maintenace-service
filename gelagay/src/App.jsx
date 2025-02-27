2bdba53847b26cff5b7fc4bf459a207ce5c29dda
abdujs <abdu12.aau@gmail.com>
2025-02-28 01:05:34 +0300

    Refactor App component to use React Router for navigation and improve structure

--- gelagay/src/App.jsx
+++ gelagay/src/App.jsx
@@ -1,13 +1,33 @@
-import './App.css'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './pages/Navbar';
+import Hero from './pages/Hero';
+import Why from './pages/why';
+import Services from './pages/Services';
+import Footer from './pages/Footer';
+import ContactForm from './components/ContactForm';
+import BookingForm from './components/BookingForm';
+import Signup from './components/SignUpForm';
+import Login from './components/Login';
 
-function App() {
+const App = () => (
+  <div>
+    <Navbar />
+    <Hero />
+    <Why />
+    <Services />
+    <ContactForm />
+    <Routes>
+      {/* <Route path="/" element={<Hero />} />
+      <Route path="/about" element={<About />} /> */}
+      {/* <Route path="/services" element={<Services />} /> */}
+      {/* <Route path="/contact" element={<ContactForm />} /> */}
+      <Route path="/BookingForm" element={<BookingForm />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+    <Footer />
+  </div>
+);
 
-  return (
-    <>
-      
-        
-    </>
-  )
-}
-
-export default App
+export default App;
